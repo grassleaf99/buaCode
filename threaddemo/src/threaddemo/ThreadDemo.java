bf00b4e8daf73478247f3614bea81edbe611e1cb
grassleaf99 <65818460+grassleaf99@users.noreply.github.com>
2020-12-07 17:03:11 +0700

    code thread javaswing

---
+++ threaddemo/src/threaddemo/ThreadDemo.java
@@ -0,0 +1,26 @@
+
+package threaddemo;
+
+
+public class ThreadDemo extends Thread {
+    private int index;
+    public ThreadDemo(String name){
+        super(name);
+    }
+    @Override
+    public void run(){
+        System.out.println("start "+getName());
+        for (int i = 0; i < 7; i++) {
+            
+        
+
+            System.out.println("inside "+getName()+(index++));
+            try{
+               sleep(1000);
+            }catch(InterruptedException e){
+                System.out.println(e);
+            }
+        }
+        System.out.println("the end "+getName());
+    }
+}
